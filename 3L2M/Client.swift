9032371816dfb8fb63cfeab0d767ace8ecb1189d
tipoLol <negroman11@mail.ru>
2023-01-05 14:38:01 -0800

    kljb

---
+++ 3L2M/Client.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+//class Client {
+//    var name: String
+//    var surname: String
+//    var age: Int
+//    var balance: Int
+//    
+//    init() {
+//        self.name = ""
+//        self.surname = ""
+//        self.age = 0
+//        self.balance = 0
+//    }
+//    
+//    init(name: String, surname: String, age: Int, balance: Int){
+//        self.name = name
+//        self.surname = surname
+//        self.age = age
+//        self.balance = balance
+//    }
+//    
+//}
---
+++ 3L2M/Pon.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+class Inter {
+    var name: String
+    var strana: String
+    var opisanie: Int
+    var data: Int
+    var aftor: String
+    
+    init(name: String, strana: String, opisanie: Int, data: Int) {
+        self.name = name
+        self.strana = strana
+        self.opisanie = opisanie
+        self.data = data
+        
+        var stran = "Kyrgyzstan"
+        let city = ""
+        func Array() {
+            if city == "Kyrgyzstan" {
+                print("")
+                if city == "Bishkek" {
+                    print("Today in the \(stran) in the Bishkek, avariya boldu")
+                    
+                }
+            }
+        }
+    }
+}
